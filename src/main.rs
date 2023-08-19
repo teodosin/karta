17356570d2bf3bb4c9f9ac3bb71d985b2fc2be9b
teodosin <teodosin@protonmail.com>
2023-08-19 15:17:12 +0300

    Update to Bevy 0.11 as a test. Remove bevy_mod_picking, as it had not yet been updated. The only change that had to be made is that the mouseScrollEvent had to derive the Event macro.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,19 @@
 use std::collections::HashMap;
 
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle, input::mouse::MouseWheel};
-use bevy_egui::{egui, EguiPlugin, EguiContexts};
-use bevy_mod_picking::{PickableBundle, prelude::{RaycastPickCamera, RaycastPickTarget, OnPointer, Drag}, DefaultPickingPlugins};
+use bevy::{input::mouse::MouseWheel, prelude::*, sprite::MaterialMesh2dBundle};
 use rand::Rng;
 
-
 fn main() {
-
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(DefaultPickingPlugins)
-        .add_plugin(EguiPlugin)
-
         .insert_resource(ViewSettings::default())
-
         .add_startup_system(setup)
         .add_startup_system(spawn_random_nodes)
-
         .add_event::<MouseScrollEvent>()
-
         .add_system(mouse_scroll_events)
-
         .add_system(graph_zoom)
         .add_system(spread_nodes)
-    
         .run();
-    
 }
 
 #[derive(Resource, Debug)]
@@ -36,16 +23,12 @@ struct ViewSettings {
 
 impl Default for ViewSettings {
     fn default() -> Self {
-        ViewSettings {
-            zoom: 1.0,
-        }
+        ViewSettings { zoom: 1.0 }
     }
 }
 
 #[derive(Resource, Default, Debug)]
-struct InputSettings {
-
-}
+struct InputSettings {}
 
 #[derive(Component)]
 struct Node;
@@ -59,16 +42,11 @@ struct GraphPosition(Vec3);
 #[derive(Component)]
 struct GraphColor(Color);
 
-fn setup(
-    mut commands: Commands,
-) {
-    commands.spawn((
-        Camera2dBundle::default(),
-        RaycastPickCamera::default()
-    ));
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
 
-fn create_window(mut commands: Commands){
+fn create_window(mut commands: Commands) {
     commands.spawn(Window {
         title: "Karta".to_string(),
         ..default()
@@ -79,8 +57,7 @@ fn spawn_random_nodes(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-){
-
+) {
     for i in 0..10 {
         let mut rng = rand::thread_rng();
 
@@ -92,73 +69,62 @@ fn spawn_random_nodes(
                 transform: Transform::from_translation(Vec3::new(
                     rng.gen_range(-400.0..400.0),
                     rng.gen_range(-400.0..400.0),
-                    i as f32)),
+                    i as f32,
+                )),
                 ..default()
             },
-            PickableBundle::default(),
-            RaycastPickTarget::default(),
-            OnPointer::<Drag>::target_component_mut::<Transform>(|drag, transform| {
-                transform.translation += drag.delta.extend(0.0);
-            })
         ));
     }
 }
 
+#[derive(Event)]
 struct MouseScrollEvent {
-    value: f32
+    value: f32,
 }
 
 fn mouse_scroll_events(
     mut scroll_evr: EventReader<MouseWheel>,
     mut ev_mouse_scroll: EventWriter<MouseScrollEvent>,
     mut view_settings: ResMut<ViewSettings>,
-){
+) {
     use bevy::input::mouse::MouseScrollUnit;
-    for ev in scroll_evr.iter(){
+    for ev in scroll_evr.iter() {
         match ev.unit {
             MouseScrollUnit::Line => {
                 view_settings.zoom = ev.y;
-                ev_mouse_scroll.send(MouseScrollEvent {
-                    value: ev.y
-                });
-            },
-            MouseScrollUnit::Pixel => {
-                ()
+                ev_mouse_scroll.send(MouseScrollEvent { value: ev.y });
             }
+            MouseScrollUnit::Pixel => (),
         }
     }
 }
 
 fn graph_zoom(
-    mut query: Query<&mut OrthographicProjection, With<Camera>>, 
+    mut query: Query<&mut OrthographicProjection, With<Camera>>,
     time: Res<Time>,
-    mut events: EventReader<MouseScrollEvent>
+    mut events: EventReader<MouseScrollEvent>,
 ) {
     let zoom_mult: f32 = 2.;
 
-    for event in events.iter(){
-        for mut projection in query.iter_mut(){
+    for event in events.iter() {
+        for mut projection in query.iter_mut() {
             let mut log_scale = projection.scale.ln();
             log_scale -= event.value * zoom_mult * time.delta_seconds();
             projection.scale = log_scale.exp();
-    
+
             println!("Current zoom scale: {}", projection.scale);
         }
     }
 }
 
-
-
-fn spread_nodes(
-    mut query: Query<(Entity, &Node, &mut Transform)>,
-) {
+fn spread_nodes(mut query: Query<(Entity, &Node, &mut Transform)>) {
     let mut velocities: HashMap<Entity, Vec2> = HashMap::new();
 
-    for (esrc, _src_node, transform) in query.iter(){
+    for (esrc, _src_node, transform) in query.iter() {
         let src_pos: Vec2 = Vec2::new(transform.translation.x, transform.translation.y);
         let mut vel: Vec2;
 
-        for (etrgt, _trgt_node, transform) in query.iter(){
+        for (etrgt, _trgt_node, transform) in query.iter() {
             let trgt_pos: Vec2 = Vec2::new(transform.translation.x, transform.translation.y);
 
             vel = trgt_pos - src_pos;
@@ -167,12 +133,11 @@ fn spread_nodes(
                 velocities.insert(esrc, vel.normalize() * 0.0000000000000000001);
                 velocities.insert(etrgt, vel.normalize() * -0.0000000000000000001);
             }
-        };
-    };
+        }
+    }
 
-    for (e, _src_node, mut transform) in query.iter_mut(){
+    for (e, _src_node, mut transform) in query.iter_mut() {
         let vel = velocities.get(&e).unwrap_or(&Vec2::ZERO);
         transform.translation += Vec3::new(0.01, 0.01, 0.0);
-    };
-    
+    }
 }
