70e1c46b6acb3ff208fbae16991f5219b97e0506
teodosin <teodosin@protonmail.com>
2023-09-13 10:00:50 +0300

    Directory hopping

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,33 @@
 //lib
-use std::collections::HashMap;
 
 use bevy::{input::mouse::*, prelude::*};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_mod_picking::prelude::*;
 
+pub mod tools;
+
 mod context;
+mod scene;
 
 use crate::context::ContextPlugin;
+use crate::scene::ScenePlugin;
 
 pub fn karta_app() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(DefaultPickingPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(DefaultPickingPlugins
+            .build()
+            .disable::<DebugPickingPlugin>()
+        )        
+        //.add_plugins(DefaultPlugins)
+        .add_plugins(WorldInspectorPlugin::new())
 
         .add_plugins(ContextPlugin)
+        .add_plugins(ScenePlugin)
 
         .insert_resource(ViewSettings::default())
+        .insert_resource(ViewData::default())
+        .insert_resource(InputData::default())
 
         .add_systems(Startup, setup)
 
@@ -26,7 +38,7 @@ pub fn karta_app() {
         .add_systems(Update, graph_zoom)
         .add_systems(Update, graph_pan)
 
-        .add_systems(Update, spread_nodes)
+        .add_systems(PreUpdate, update_cursor_info)
 
         .run();
 }
@@ -42,6 +54,59 @@ impl Default for ViewSettings {
     }
 }
 
+#[derive(Resource, Debug)]
+pub struct ViewData {
+    top_z: f32,
+}
+
+impl Default for ViewData {
+    fn default() -> Self {
+        ViewData { top_z: 0.0 }
+    }
+}
+
+
+#[derive(Resource, Debug)]
+struct InputData {
+    pub latest_target_entity: Option<String>,
+    pub drag_position: Vec2,
+    pub prev_position: Vec2,
+    pub curr_position: Vec2,
+}
+
+impl Default for InputData {
+    fn default() -> Self {
+        InputData {
+            latest_target_entity: None,
+            drag_position: Vec2::ZERO,
+            prev_position: Vec2::ZERO,
+            curr_position: Vec2::ZERO,
+        }
+    }
+}
+
+fn update_cursor_info(
+    mut cursor_history: ResMut<InputData>,
+    mouse: Res<Input<MouseButton>>,
+    window: Query<&Window>,
+    camera_q: Query<(&Camera, &GlobalTransform), With<Camera2d>>,
+) {
+    let (camera, camera_transform) = camera_q.single();
+
+    cursor_history.prev_position = cursor_history.curr_position;
+
+    if mouse.just_pressed(MouseButton::Middle) {
+        cursor_history.drag_position = cursor_history.curr_position;
+    }
+
+    if let Some(world_position) = window.single().cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        cursor_history.curr_position = world_position;
+    }
+}
+
 #[derive(Resource, Default, Debug)]
 struct InputSettings {}
 
@@ -49,10 +114,26 @@ struct InputSettings {}
 struct Selected;
 
 #[derive(Component)]
-pub struct GraphNode;
+pub struct GraphNode {
+    pub path: String,
+}
 
 #[derive(Component)]
-struct Edge;
+pub struct GraphNodeEdges {
+    pub edges: Vec<Entity>,
+}
+
+#[derive(Component)]
+pub struct GraphEdge {
+    pub from: Entity,
+    pub to: Entity,
+    pub attributes: Vec<GraphEdgeAttribute>,
+}
+
+pub struct GraphEdgeAttribute {
+    pub name: String,
+    pub value: f32,
+}
 
 #[derive(Component)]
 struct GraphPosition(Vec3);
@@ -70,65 +151,89 @@ fn setup(mut commands: Commands) {
     commands.spawn((
         Camera2dBundle {
             camera_2d: Camera2d {
-                clear_color: ClearColorConfig::Custom(Color::rgb(0.1, 0.1, 0.1)),
+                clear_color: ClearColorConfig::None,
+                ..default()
             },
-            ..Default::default()
+            camera: Camera {
+                order: 1,
+                ..default()
+            },
+            ..default()
         },
-        RaycastPickCamera::default()
+        RaycastPickCamera::default(),
     ));
+    commands.spawn(Camera3dBundle {
+        camera: Camera {
+            order: 0,
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 6., 12.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        ..default()
+    });
 }
 
 
 fn move_node_selection(
     mut ev_mouse_drag: EventReader<MoveNodesEvent>,
     mouse: Res<Input<MouseButton>>,
-    view_settings: Res<ViewSettings>,
-    mut query: Query<(Entity, &GraphNode, &mut Transform), With<Selected> >,
+    cursor: Res<InputData>,
+    mut query: Query<(Entity, &GraphNode, &mut Transform), With<Selected>>,
+    mut view_data: ResMut<ViewData>,
 ) {
-    for ev in ev_mouse_drag.iter() {
+
+    if mouse.just_pressed(MouseButton::Left) {
+        for (_entity, _node, mut transform) in query.iter_mut() {
+            transform.translation.z = 60.0;
+            view_data.top_z += 1.0;
+        }
+    }
+
+    for _ev in ev_mouse_drag.iter() {
         if mouse.pressed(MouseButton::Left){
             for (_entity, _node, mut transform) in query.iter_mut() {
-                transform.translation.x += ev.delta.x * view_settings.zoom;
-                transform.translation.y -= ev.delta.y * view_settings.zoom;
+                    transform.translation.x += cursor.curr_position.x - cursor.prev_position.x;
+                    transform.translation.y += cursor.curr_position.y - cursor.prev_position.y;     
             }
         }
+        break
     }
 }
 
 #[derive(Event)]
-struct MoveNodesEvent {
-    delta: Vec2,
-}
+struct MoveNodesEvent;
 
 impl From<ListenerInput<Pointer<Drag>>> for MoveNodesEvent {
-    fn from(event: ListenerInput<Pointer<Drag>>) -> Self {
-        MoveNodesEvent {
-            delta: event.delta, 
-        }
+    fn from(_event: ListenerInput<Pointer<Drag>>) -> Self {
+        MoveNodesEvent
     }
 }
 
 fn graph_pan(
-    mut query: Query<(&mut Transform, &OrthographicProjection), With<Camera>>,
+    mut query: Query<&mut Transform, With<Camera2d>>,
     windows: Query<&Window>,
-    mut mot_events: EventReader<MouseMotion>,
-    click_evr: Res<Input<MouseButton>>,
+    mouse: Res<Input<MouseButton>>,
+    view_settings: Res<ViewSettings>,
+    _cursor: Res<InputData>,
+    mut motion: EventReader<MouseMotion>,
 ) {
     let window = windows.single();
     let _cursor = window.cursor_position();
 
-    if click_evr.pressed(MouseButton::Middle) {
-        for event in mot_events.iter() {
-            for (mut transform, ortho) in query.iter_mut() {
-                transform.translation.x -= event.delta.x * ortho.scale;
-                transform.translation.y += event.delta.y * ortho.scale;
-            }
+    if mouse.pressed(MouseButton::Middle) {
+        for mut transform in query.iter_mut() {
+                // transform.translation.x -= cursor.curr_position.x - cursor.prev_position.x;
+                // transform.translation.y -= cursor.curr_position.y - cursor.prev_position.y; 
+            for ev in motion.iter() {
+                transform.translation.x -= ev.delta.x * view_settings.zoom;
+                transform.translation.y += ev.delta.y * view_settings.zoom;
+            }      
         }
+        
     }
 }
 
 fn graph_zoom(
-    mut query: Query<&mut OrthographicProjection, With<Camera>>,
+    mut query: Query<&mut OrthographicProjection, With<Camera2d>>,
     mut view_settings: ResMut<ViewSettings>,
     time: Res<Time>,
     mut events: EventReader<MouseWheel>,
@@ -148,31 +253,5 @@ fn graph_zoom(
             }},
             MouseScrollUnit::Pixel => (),
         }
-    }
-}
-
-fn spread_nodes(mut query: Query<(Entity, &Node, &mut Transform)>) {
-    let mut velocities: HashMap<Entity, Vec2> = HashMap::new();
-
-    for (esrc, _src_node, transform) in query.iter() {
-        let src_pos: Vec2 = Vec2::new(transform.translation.x, transform.translation.y);
-        let mut vel: Vec2;
-
-        for (etrgt, _trgt_node, transform) in query.iter() {
-            let trgt_pos: Vec2 = Vec2::new(transform.translation.x, transform.translation.y);
-
-            vel = trgt_pos - src_pos;
-
-            if vel.length() < 100.0 {
-                velocities.insert(esrc, vel.normalize() * 0.00001);
-                velocities.insert(etrgt, vel.normalize() * -0.00001);
-            }
-        }
-    }
-
-    for (e, _src_node, mut _transform) in query.iter_mut() {
-        let _vel = velocities.get(&e).unwrap_or(&Vec2::ZERO);
-        debug!("{:?}", "bozo".to_string());
-        //transform.translation += Vec3::new(0.1, 0.1, 0.0);
     }
 }
