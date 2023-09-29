8b4901f73efaf54b95fa116a5fd53eb0612f8fb6
teodosin <teodosin@protonmail.com>
2023-09-29 13:12:08 +0300

    Remove accidentally pasted code that shouldn't be there

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,4 @@ use karta::karta_app;
 
 fn main(){
     karta_app();
-}                            // Zoom-in is centered on mouse position
-let amount = zoom_mult - 1.0;
-let adjusted_position = (input_data.curr_position - transform.translation.truncate()) * amount;
-transform.translation.x += adjusted_position.x;
-transform.translation.y += adjusted_position.y;
+}
