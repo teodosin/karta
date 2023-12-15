096e7765e4ee793dae2ba27e0df647c7d0f17a40
teodosin <teodosin@protonmail.com>
2023-12-15 16:30:34 +0200

    Add node debug labels to visualize node information

--- src/tests/world_utils.rs
+++ src/tests/world_utils.rs
@@ -1,6 +1,13 @@
 #[cfg(test)]
 pub mod world_utils{
-    pub fn create_karta_world(){
-        
+    use bevy::app::App;
+
+    pub fn create_karta_world() -> App {
+        let mut app = App::new();
+
+        // app
+            
+
+        app
     }
 }
