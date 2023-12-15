def34cbb5b4e562b4fe39a10971f4e5512cf0999
teodosin <teodosin@protonmail.com>
2023-12-15 18:07:06 +0200

    Refactor graphedge to use paths instead of entities

--- src/graph/edges.rs
+++ src/graph/edges.rs
@@ -1,12 +1,12 @@
 
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf};
 
 use bevy::prelude::*;
 use serde::{Deserialize, Serialize};
 
 use crate::{graph::attribute::Attributes, events::edges::EdgeSpawnedEvent};
 
-use super::{nodes::{GraphDataNode, GraphNodeEdges}, graph_cam::ViewData};
+use super::{nodes::{GraphDataNode, GraphNodeEdges}, graph_cam::ViewData, context::PathsToEntitiesIndex};
 
 pub struct EdgesPlugin;
 
@@ -30,8 +30,8 @@ impl Plugin for EdgesPlugin {
 // A component for the most basic data of an EDGE
 #[derive(Component, Reflect)]
 pub struct GraphEdge {
-    pub source: Entity,
-    pub target: Entity,
+    pub source: PathBuf,
+    pub target: PathBuf,
 }
 
 impl GraphEdge {
@@ -64,19 +64,17 @@ impl Default for EdgeTypes {
 // And use EdgeDefaults resource to set the default length
 pub fn create_edge(
     event: &mut EventWriter<EdgeSpawnedEvent>,
-    from: &Entity, 
-    to: &Entity, 
+    from: &PathBuf, 
+    to: &PathBuf, 
     etype: EdgeTypes,
     commands: &mut Commands,
     edges: &Query<(&GraphEdge, &EdgeType)>,
 ){
     // Check if an edge already exists between the node pair
-
-
     for (edge, edge_type) in edges.iter() {
         if edge.same_pair(&GraphEdge {
-            source: *from,
-            target: *to,
+            source: (*from).to_path_buf(),
+            target: (*to).to_path_buf(),
         }) {
             println!("Edge already exists between {:?} and {:?}", from, to);
             return
@@ -96,8 +94,8 @@ pub fn create_edge(
 
     let edge = commands.spawn((
         GraphEdge {
-            source: *from,
-            target: *to,
+            source: (*from).to_path_buf(),
+            target: (*to).to_path_buf(),
         },
         EdgeType {
             etype,
@@ -114,41 +112,88 @@ pub fn create_edge(
     });
 }
 
+/// Function to update the GraphNodeEdges component of nodes when an edge is spawned
+/// The event could possibly be modified to include the source and target paths, 
+/// to simplify the function. 
 pub fn add_edge_to_node_indexes(
     mut event: EventReader<EdgeSpawnedEvent>,
     mut edges: Query<&GraphEdge>,
-    nodes: Query<Entity, With<GraphNodeEdges>>,
+    nodes: Query<&GraphDataNode, With<GraphNodeEdges>>,
+    pe_index: Res<PathsToEntitiesIndex>,
     mut node_edges: Query<&mut GraphNodeEdges>,
 ) {
     for ev in event.read(){
         let edge_entity = ev.entity;
         let edge_data = edges.get_mut(edge_entity).unwrap();
 
-        let mut source_edges = match node_edges.get_mut(edge_data.source){
+
+        let source_entity = match pe_index.0.get(&edge_data.source){
+            Some(entity) => entity,
+            None => {continue},
+        };
+        let mut source_edges = match node_edges.get_mut(*source_entity){
             Ok(edges) => edges,
             Err(_) => {continue},
         };
-        let target_entity = nodes.get(edge_data.target).unwrap();
 
-        source_edges.add_edge(target_entity, edge_entity);
+        source_edges.insert_edge(edge_data.target.clone(), edge_entity);
  
-        let mut target_edges = match node_edges.get_mut(edge_data.target){
+        let target_entity = match pe_index.0.get(&edge_data.target){
+            Some(entity) => entity,
+            None => {continue},
+        };
+        let mut target_edges = match node_edges.get_mut(*target_entity){
             Ok(edges) => edges,
             Err(_) => {continue},
         };
-        let source_entity = nodes.get(edge_data.source).unwrap();
 
-        target_edges.add_edge(source_entity, edge_entity);
+        target_edges.insert_edge(edge_data.source.clone(), edge_entity);
+    }
+}
+
+pub fn update_node_indexes(
+    mut edges: Query<(Entity, &GraphEdge)>,
+    nodes: Query<&GraphDataNode, With<GraphNodeEdges>>,
+    mut node_edges: Query<&mut GraphNodeEdges>,
+    pe_index: Res<PathsToEntitiesIndex>,
+){
+    for (edge_entity, edge_data) in edges.iter_mut(){
+        let source_entity = match pe_index.0.get(&edge_data.source){
+            Some(entity) => entity,
+            None => {continue},
+        };
+        let mut source_edges = match node_edges.get_mut(*source_entity){
+            Ok(edges) => edges,
+            Err(_) => {continue},
+        };
+
+        source_edges.insert_edge(edge_data.target.clone(), edge_entity);
+
+        let target_entity = match pe_index.0.get(&edge_data.target){
+            Some(entity) => entity,
+            None => {continue},
+        };
+        let mut target_edges = match node_edges.get_mut(*target_entity){
+            Ok(edges) => edges,
+            Err(_) => {continue},
+        };
+
+        target_edges.insert_edge(edge_data.source.clone(), edge_entity);
     }
 }
 
 pub fn despawn_edges(
     mut commands: Commands,
     mut edges: Query<(Entity, &GraphEdge)>,
-    nodes: Query<&GraphNodeEdges>,
+    pe_index: Res<PathsToEntitiesIndex>,
 ) {
     for (edge_entity, edge_data) in edges.iter_mut() {
-        if nodes.get(edge_data.source).is_err() || nodes.get(edge_data.target).is_err() {
+        let source_entity = pe_index.0.get(&edge_data.source);
+        let target_entity = pe_index.0.get(&edge_data.target);
+
+        println!("Checking if {:?} and {:?} exist", source_entity, target_entity);
+
+        if source_entity.is_none() || target_entity.is_none() {
             println!("Despawning edge");
             commands.entity(edge_entity).despawn_recursive();
 
