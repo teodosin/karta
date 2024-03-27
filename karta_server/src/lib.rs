2b5e40ee2ab91c77ef54a72306d1beadbfb4fae1
Viktor <teodosin@protonmail.com>
2024-03-27 13:52:20 +0200

    Create first two tests

--- karta_server/src/lib.rs
+++ karta_server/src/lib.rs
@@ -1,71 +1,116 @@
 use std::path::PathBuf;
 
-use agdb::{UserValue, DbId};
+use agdb::{DbId, QueryBuilder, UserValue};
+use path_ser::buf_to_str;
 
-/// The main graph structure to be interacted with. 
+mod path_ser;
+
+/// The main graph structure to be interacted with.
 struct Graph {
+    /// The name of the application using this library.
+    name: String,
+
     /// AGDB database
     db: agdb::Db,
 
-    /// Path to the root directory of the graph. 
+    /// Path to the root directory of the graph.
     /// All paths are relative to this root.
     root_path: std::path::PathBuf,
 
     /// Path to the where the db is stored in the file system.
+    /// Either default for the operating system (as determined by the directories crate) or custom.
     /// Includes the name of the directory.  
-    storage_path: std::path::PathBuf,
+    storage_path: StoragePath,
 
     /// Whether the library should maintain readable files for the nodes
     /// in the graph.
-    /// 
+    ///
     /// If true, there will be a directory at the storage path which
-    /// mirrors the directory structure starting from the root path. 
+    /// mirrors the directory structure starting from the root path.
+    /// TODO: Should this be behind a feature flag?
     maintain_readable_files: bool,
 }
 
+enum StoragePath {
+    Default,
+    Custom(PathBuf),
+}
+
 /// Agdb has multiple implementations. If the size of the database is small enough, it can be stored in memory.
-/// If the database is too large, it can be stored in a file. 
-/// TODO: Not in use currently. 
-enum graph_db {
+/// If the database is too large, it can be stored in a file.
+/// TODO: Not in use currently.
+enum GraphDb {
     Mem(agdb::Db),
     File(agdb::DbFile),
 }
 
 impl Graph {
-
     /// Constructor. Panics if the db cannot be created.
+    ///
+    /// Takes the desired root of the graph as a parameter and the name for the db.
+    ///
+    /// Creates the db at the storage_path, or initialises the db if it already exists there.
+    ///
     /// TODO: Add error handling.
-    fn new(
-        root_path: &str, 
-        storage_path: &str, 
-    ) -> Self {
-        let db = agdb::Db::new(storage_path);
+    fn new(root_path: PathBuf, name: &str) -> Self {
+        let storage_path = directories::ProjectDirs::from("com", "fs_graph", name)
+            .unwrap()
+            .data_dir()
+            .to_path_buf();
 
-        Graph { 
-            db: db.expect("Failed to create db"), 
-            root_path: root_path.into(), 
-            storage_path: storage_path.into(),
+        // Create the path if it doesn't exist
+        if !storage_path.exists() {
+            std::fs::create_dir_all(&storage_path).expect("Failed to create storage path");
+        }
+
+        let db = agdb::Db::new(storage_path.join(name).to_str().unwrap());
+
+        let mut db = db.expect("Failed to create db");
+
+        let _ = db.exec_mut(&QueryBuilder::insert().nodes().aliases("root").query());
+
+        Graph {
+            name: name.to_string(),
+            db,
+            root_path: root_path.into(),
+            storage_path: StoragePath::Default,
             maintain_readable_files: false,
         }
     }
 
-    fn maintain_readable_files(&self, maintain: bool) {
+    /// Alternate constructor. Use this if you want to set a custom storage path for the db. Panics if the db cannot be created
+    fn new_custom_storage(root_path: PathBuf, name: &str, storage_path: PathBuf) -> Self {
+        let db = agdb::Db::new(storage_path.join(name).to_str().unwrap());
+
+        let mut db = db.expect("Failed to create db");
+
+        let _ = db.exec_mut(&QueryBuilder::insert().nodes().aliases("root").query());
+
+        Graph {
+            name: name.to_string(),
+            db,
+            root_path: root_path.into(),
+            storage_path: StoragePath::Custom(storage_path),
+            maintain_readable_files: false,
+        }
+    }
+
+    /// Set whether the library should maintain readable files for the nodes in the graph.
+    fn maintain_readable_files(&mut self, maintain: bool) {
         self.maintain_readable_files = maintain;
     }
 
     /// Opens the connections of a particular node.
-    /// Insert the path to the node relative to the root of the graph.
-    fn open_node(&self, node_path: &str) {
-
+    /// Takes in the path to the node relative to the root of the graph.
+    fn open_node(&self, path: PathBuf) {
         // Resolve the full path to the node
-        let full_path: PathBuf = self.root_path.join(node_path);
+        let full_path = self.root_path.join(path);
 
         // Check if the node is physical
         if full_path.exists() {
-
-            return Ok(());
+            ()
         } else {
-            return Ok(());
+            ()
         }
     }
 
@@ -82,7 +127,7 @@ impl Graph {
 struct Node {
     db_id: Option<DbId>,
     name: String,
-    attributes: Vec<Attribute>,
+    //attributes: Vec<Attribute>,
 }
 
 struct Edge {
@@ -99,14 +144,103 @@ struct Attribute {
 // -----------------------------------------------------------------------------
 #[cfg(test)]
 mod tests {
+    use agdb::QueryResult;
+    use directories::ProjectDirs;
+
     use super::*;
+
+    /// Setup function for tests. Always stores the db in the data_dir.
+    fn setup(test_name: &str) -> Graph {
+        let name = format!("fs_graph_test_{}", test_name);
+        let root = ProjectDirs::from("com", "fs_graph", &name)
+            .unwrap()
+            .data_dir()
+            .to_path_buf();
+        let full_path = root.join(&name);
+
+        let graph = Graph::new(root.clone().into(), &name);
+
+        assert_eq!(
+            full_path.exists(),
+            true,
+            "Test directory has not been created"
+        );
+
+        graph
+    }
+
+    /// Cleanup function for tests. Removes the root directory from the data_dir.
+    fn cleanup(test_name: &str) {
+        // Uncomment this return only if you need to temporarily look at the contents
+        // return;
+
+        let name = format!("fs_graph_test_{}", test_name);
+        let root = ProjectDirs::from("com", "fs_graph", &name)
+            .unwrap()
+            .data_dir()
+            .to_path_buf();
+
+        std::fs::remove_dir_all(root).expect("Failed to remove root directory");
+    }
 
     #[test]
     fn test_new_graph() {
-        let graph = Graph::new("root", "storage");
-        assert_eq!(graph.root_path, PathBuf::from("root"));
-        assert_eq!(graph.storage_path, PathBuf::from("storage"));
+        let func_name = "test_new_graph";
+
+        let name = format!("fs_graph_test_{}", func_name);
+        let root = ProjectDirs::from("com", "fs_graph", &name)
+            .unwrap()
+            .data_dir()
+            .to_path_buf();
+
+        println!("Expected full path: {:?}", root);
+
+        let graph = Graph::new(root.clone().into(), &name);
+
+        println!("Size of graph: {:?} bytes", graph.db.size());
+
+        assert_eq!(root.exists(), true, "Root directory does not exist");
+
+        // Check that there exists a root node
+        let root_node_result = graph.db.exec(&QueryBuilder::select().ids("root").query());
+
+        match root_node_result {
+            Ok(root_node) => {
+                assert_eq!(root_node.result /* expected value */, 1);
+            }
+            Err(e) => {
+                println!("Failed to execute query: {}", e);
+            }
+        }
+
+        cleanup(func_name);
+    }
+
+    #[test]
+    fn existing_db_in_directory() {
+        // We add a node to the db, then create a new graph with the same name.
+        // The new graph should be able to access the node.
+        let func_name = "existing_db_in_directory";
+        let mut first = setup(func_name);
+
+        let _ = first.db.exec_mut(&QueryBuilder::insert().nodes().aliases("testalias").query());
+
+
+        let second = setup(func_name);
+
+        let root_node_result = second.db.exec(&QueryBuilder::select().ids("testalias").query());
+
+        match root_node_result {
+            Ok(root_node) => {
+                assert_eq!(root_node.result /* expected value */, 1);
+            }
+            Err(e) => {
+                println!("Failed to execute query: {}", e);
+            }
+        }
+
+        assert_eq!(true, true);
+
+        cleanup(func_name);
     }
 }
-
-
---
+++ karta_server/src/path_ser.rs
@@ -0,0 +1,17 @@
+// Utility functions for serializing and deserializing paths.
+// These should be expanded to correctly handle paths that are not utf-8.
+// The database should be able to be transported between different operating systems.
+
+// NOTE: The handling of non-utf8 paths is not implemented yet. The functions exist and
+// are to be used in the code so that their internal implementation can be changed later.
+
+use std::path::PathBuf;
+
+// TODO: Actually handle non-utf8 paths.
+pub fn str_to_buf(str: &str) -> PathBuf {
+    PathBuf::from(str)
+}
+
+pub fn buf_to_str(buf: PathBuf) -> String {
+    buf.to_str().unwrap().into()
+}
