842b8e6e868931618de9ae015a5f9f71d1da41b3
teodosin <viktor@teodosin.com>
2025-03-16 09:58:06 +0200

    WIP server refactor

---
+++ karta_server/src/server/data_endpoints.rs
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+pub async fn index_node_connections(Extension(state): Extension<AppState>, Path(id): Path<String>) {
+    let nodepath = NodePath::from_alias(&id);
+
+    let mut graph = state.graph_commands.write().unwrap();
+
+    graph.index_node_context(&nodepath);
+}
+
+pub async fn root() -> &'static str {
+    "Welcome to Karta Server"
+}
+
+pub async fn get_all_aliases(Extension(state): Extension<AppState>) -> Json<Vec<String>> {
+    let graph = &state.graph_commands.read().unwrap();
+    let aliases = graph.get_all_aliases();
+    Json(aliases)
+}
+
+pub async fn get_root_node(Extension(state): Extension<AppState>) -> Json<Result<Node, String>> {
+    let graph = &state.graph_commands.read().unwrap();
+
+    let root_path = NodePath::root();
+    let result = graph
+        .open_node(&root_path)
+        .map_err(|e| e.to_string());
+    Json(result)
+}
+
+pub async fn get_node(
+    Extension(state): Extension<AppState>,
+    Path(id): Path<String>,
+) -> Json<Result<Node, String>> {
+    let graph = &state.graph_commands.read().unwrap();
+
+    // println!("Requested node with id: {}", id);
+    let node_path = NodePath::from_alias(&id);
+    // println!("Resulting node_path: {:#?}", node_path);
+    // println!("Resulting alias: {}", node_path.alias());
+    let result = graph
+        .open_node(&node_path)
+        .map_err(|e| e.to_string());
+    Json(result)
+}
+
+pub async fn get_node_context(
+    Extension(state): Extension<AppState>,
+    Path(id): Path<String>,
+) -> Json<Vec<(Node, Edge)>> {
+    let graph = &state.graph_commands.read().unwrap();
+    let node_path = NodePath::from_alias(&id);
+    let result = graph.open_node_connections(&node_path);
+    Json(result)
+}
