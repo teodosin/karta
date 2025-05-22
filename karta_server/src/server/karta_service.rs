0b831f79bf90d5ac34392887772b316babe793ae
teodosin <teodosin@protonmail.com>
2025-05-22 22:08:29 +0300

    open_context_from_path

--- karta_server/src/server/karta_service.rs
+++ karta_server/src/server/karta_service.rs
@@ -1,8 +1,8 @@
-use std::{path::PathBuf, sync::Arc};
+use std::{error::Error, path::PathBuf, sync::Arc};
 
 use tokio::sync::RwLock;
 
-use crate::{context::context_db::ContextDb, prelude::*};
+use crate::{context::{context::Context, context_db::ContextDb}, elements::node_path::NodeHandle, prelude::*};
 
 
 pub struct KartaService {
@@ -60,5 +60,32 @@ impl KartaService {
         &mut self.view
     }
 
+    /// Opens a context's Data and View.
+    /// This is the main function for opening a context.
+    /// Reconciles the indexed data with the physical data.
+    pub fn open_context_from_path(&self, path: NodePath) 
+        -> Result<(Vec<DataNode>, Vec<Edge>, Context), Box<dyn Error>> {
 
+        let mut finaldatanodes: Vec<DataNode> = Vec::new();
+        let mut finaledges: Vec<Edge> = Vec::new();
+        
+        let focal_handle: NodeHandle = NodeHandle::Path(path.clone());
+        let focal_node = self.data().open_node(&focal_handle)?;
+        let focal_uuid = focal_node.uuid().unwrap();
+
+        finaldatanodes.push(focal_node);
+
+        let datanodes = self.data().open_node_connections(&path);
+        for (node, edge) in datanodes {
+            finaldatanodes.push(node);
+            finaledges.push(edge);
+        }
+
+        let context = self.view.generate_context(
+            focal_uuid,
+            finaldatanodes.clone(),
+        );
+
+        return Ok((finaldatanodes, finaledges, context));
+    }
 }
