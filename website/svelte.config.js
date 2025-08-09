212e1a35a717d594173366bb620ae9c4b1ccb046
teodosin <teodosin@protonmail.com>
2025-08-09 22:47:12 +0300

    base

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/karta_ecosystem' : ''
+			base: process.env.NODE_ENV === 'production' ? '/karta' : ''
 		}
 	}
 };
